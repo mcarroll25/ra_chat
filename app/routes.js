1047aad70da1fb5c1f7bb4b427dc22cb506f9424
mcarroll25 <mikecarroll@tripointsconsulting.com>
2025-09-23 16:05:45 -0500

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,11 @@
-import { flatRoutes } from "@remix-run/fs-routes";
+import { index, route } from "@remix-run/route-config";
 
-export default flatRoutes({
-  appDirectory: "app",
-  ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"]
-});
+export default [
+  index("routes/_index.jsx"),
+  route("app", "routes/app._index.jsx"),
+  route("auth/*", "routes/auth.$.jsx"),
+  route("auth/callback", "routes/auth.callback.jsx"),
+  route("auth/login", "routes/auth.login.jsx"),
+  route("auth/token-status", "routes/auth.token-status.jsx"),
+  route("chat", "routes/chat.jsx"),
+];
