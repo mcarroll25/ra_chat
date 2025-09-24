1f4859078b5a93d95832812172cac6ae96c2ee97
mcarroll25 <mikecarroll@tripointsconsulting.com>
2025-09-23 19:12:35 -0500

    Update auth.$.jsx

--- app/routes/auth.$.jsx
+++ app/routes/auth.$.jsx
@@ -1,7 +1,7 @@
 import  shopify  from "../shopify.server";
 
 export const loader = async ({ request }) => {
-  await shopify.login(request);
+  await shopify.authenticate.admin(request);
 
   return null;
 };
